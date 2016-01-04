57c7e64a94f41a61b84c301a161eb03dd1a4b48f
Alvaro Massana <alvaromassana@gmail.com>
2016-01-04 20:24:02 +0100

    I3: number guessing game complete

--- web_guesser.rb
+++ web_guesser.rb
@@ -7,6 +7,20 @@ number = rand(100)
 
 get '/' do
 
-	erb :index, :locals => {:number => number}
+	if params["guess"].to_i > number + 5
+		answer = "Way too high!"
+
+	elsif params["guess"].to_i < number - 5
+		answer = "Way too low!"
+
+	elsif params["guess"].to_i == number
+		answer = "Right!!!"
+		
+		
+	else
+		answer = params["guess"].to_i
+	end
+
+	erb :index, :locals => {:number => number, :answer => answer}
 
 end
